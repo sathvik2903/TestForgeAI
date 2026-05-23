57ea278c96a505834c07fa96bfa123408796840a
CH Sathvik Kumar <sathvik2903@gmail.com>
2026-05-23 18:26:34 +0530

    Added requirements file

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = "mongodb+srv://sathvik:testforge123@cluster0.tabf34s.mongodb.net/?appName=Cluster0"
 
 client = AsyncIOMotorClient(MONGO_URL)
 
